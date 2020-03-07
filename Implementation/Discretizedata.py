0c73b948bc90ef32725e0ef4eb24b0781f670d70
KnethM <kmalow16@student.aau.dk>
2020-03-07 19:37:32 +0100

    review changes
    changed filepath to a new folder and added .csv files in that folder to gitignore
    
    minor changes to variable names and comments

--- Implementation/Discretizedata.py
+++ Implementation/Discretizedata.py
@@ -5,18 +5,18 @@ import time
 
 #Filepaths for movielens datadump
 #Change accordingly if needed
-ratingpath = "C:\\Users\\RedTop\\Desktop\\Movielens data\\ratings.csv"
-moviepath = "C:\\Users\\RedTop\\Desktop\\Movielens data\\movies.csv"
-movielinkpath = "C:\\Users\\RedTop\\Desktop\\Movielens data\\links.csv"
+ratingpath = "Implementation\\Movielens_data\\ratings.csv"
+moviepath = "Implemnetation\\Movielens_data\\movies.csv"
+movielinkpath = "Implementation\\Movielens_data\\links.csv"
 #API-key from OMDB Api (limit: 1000 daily)
 apikey = "ad37bdca"
 
 
 #Read the ratings csv file one line at a time, only keep the data we need
 #As output we get a csv file where each line corresponds to en edge in the graph (Head,Tail,Weight) (MovieId, UserId, Rating)
-def discratingdata():
+def disc_rating_data():
     with open(ratingpath, "r") as fp:
-        nf = open("graph.csv","w+", newline='')
+        nf = open("Implementation\\Movielens_data\\graphs.csv","w+", newline='')
         filewriter = csv.writer(nf)
 
         #rating is an array of the form [UserID,MovieID,Rating,Timestamp]
@@ -28,9 +28,9 @@ def discratingdata():
 
 #Read the movies csv file and take out the information we need, including release year
 #As output we get a csv file where each line is a node for a movie of the form (movieId,movieTitle,ReleaseYear,genres)
-def discmoviedata():
+def disc_movie_data():
     with open(moviepath, "r", encoding='utf-8') as fp:
-        nf = open("movies.csv","w+", newline='', encoding='utf-8')
+        nf = open("Implementation\\Movielens_data\\movie_nodes.csv","w+", newline='', encoding='utf-8')
         filewriter = csv.writer(nf)
 
         #File that links the movieID to an IMDBid
@@ -44,7 +44,8 @@ def discmoviedata():
             #Certain movietitles are missing the year it was released
             #if so, make api call to get the year from OMDB
             if "(" in movietitle:
-                movieyear = movietitle[-5:-1]                
+                #Take the substring from the movie title that contains the year is, it is always at the end of title as (year)
+                movieyear = movietitle[-5:-1]           
             else:
                 #link is an array of the form [movieID,imdbID,tmdbID]
                 for link in csv.reader(linkfile):
@@ -70,9 +71,9 @@ def discmoviedata():
 
 #Read the ratings csv file and convert the data into users
 #As output we get a csv file where each line is a node for a user of the form (userId, ratingcount)
-def discuserdata():
+def disc_user_data():
     with open(ratingpath, "r") as fp:
-        nf = open("users.csv","w+", newline='')
+        nf = open("Implementation\\Movielens_data\\user_nodes.csv","w+", newline='')
         filewriter = csv.writer(nf)
 
         currentid = '1'
@@ -84,9 +85,10 @@ def discuserdata():
             #skip line if not a rating
             if "user" in rating[0]:
                 pass
-            #count up if the samer user have made multiple ratings
+            #count up if the same user have made multiple ratings
             elif rating[0] == currentid:
                 counter += 1
+            #Write to file once we find a different userid, and remember to count the current row we're looking at
             else:
                 filewriter.writerow([currentid,counter])
                 currentid = rating[0]
