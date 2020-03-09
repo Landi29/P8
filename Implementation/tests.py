cd74ade30b6ce987960a1f01ad5eb6a162efd7e1
Aryan <amoha16@student.aau.dk>
2020-03-09 12:41:16 +0100

    Added a test to see if the filepaths work.

--- Implementation/tests.py
+++ Implementation/tests.py
@@ -1,13 +1,35 @@
 # The tests can both be run with python and pytest.
 import Discretizedata
+import csv
 import unittest
+import os
 
 # The test class for graph methods.
 class TestGraphMethods(unittest.TestCase):
-    # An example unit test method. 
-    
-    def test_1(self):
-        self.assertTrue(True)
+    RATING_DATA = [
+    "userId,movieId,rating,timestamp",
+    "1,296,5.0,1147880044",
+    "1,306,3.5,1147868817",
+    "1,307,5.0,1147868828",
+    "1,665,5.0,1147878820",
+    "1,899,3.5,1147868510",
+    "1,1088,4.0,1147868495",
+    "1,1175,3.5,1147868826",
+    "1,1217,3.5,1147878326",
+    "1,1237,5.0,1147868839"
+    ]
+    # (MovieId, UserId, Rating)
+    EXPECTED_VALUE = "296,1,5.0\n"
+    graph_data = None
+    with open(Discretizedata.RATINGPATH, 'w') as writer:
+       for line in RATING_DATA:
+           writer.write(line + os.linesep)
+        
+    def test_disc_rating_data(self):
+        Discretizedata.disc_rating_data()
+        with open(Discretizedata.GRAPH_DATA_PATH, 'r') as reader:
+            self.graph_data = reader.readlines()
+        self.assertEqual(self.graph_data[1], self.EXPECTED_VALUE)
 
 if __name__ == "__main__":
     unittest.main()
