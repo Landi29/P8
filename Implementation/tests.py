ac0a42574e311cb383ec74e6fa422b112cebf7f2
Aryan <amoha16@student.aau.dk>
2020-03-09 13:54:41 +0100

    Test know uses all the indices in the array.

--- Implementation/tests.py
+++ Implementation/tests.py
@@ -12,11 +12,6 @@ class TestDiscretizedata(unittest.TestCase):
         "1,306,3.5,1147868817",
         "1,307,5.0,1147868828",
         "1,665,5.0,1147878820",
-        "1,899,3.5,1147868510",
-        "1,1088,4.0,1147868495",
-        "1,1175,3.5,1147868826",
-        "1,1217,3.5,1147878326",
-        "1,1237,5.0,1147868839"
     ]
 
     with open(Discretizedata.RATINGPATH, 'w') as writer:
@@ -24,15 +19,20 @@ class TestDiscretizedata(unittest.TestCase):
             writer.write(line + os.linesep)
 
     def test_disc_rating_data(self):
-        """ Tests the disc_rating_data() method by using the first 10 lines
+        """ Tests the disc_rating_data() function by using the first 5 lines
          in the ratings.csv file"""
-         # expected_value =  (MovieId, UserId, Rating)
-        expected_value = "296,1,5.0\n"
+        expected_value = [
+            "296,1,5.0\n",
+            "306,1,3.5\n",
+            "307,1,5.0\n",
+            "665,1,5.0\n"
+        ]
         graph_data = None
+        
         Discretizedata.disc_rating_data()
         with open(Discretizedata.GRAPH_DATA_PATH, 'r') as reader:
             graph_data = reader.readlines()
-        self.assertEqual(graph_data[1], expected_value)
+        self.assertEqual(graph_data[1:], expected_value)
 
 if __name__ == "__main__":
     unittest.main()
