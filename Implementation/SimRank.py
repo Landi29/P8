090c97a5c2d6fd73499e08bcf4568e592e51c208
Andreas Dahl Nielsen <andnie16@student.aau.dk>
2020-04-06 09:28:00 +0200

    Update SimRank.py

--- Implementation/SimRank.py
+++ Implementation/SimRank.py
@@ -9,45 +9,50 @@ class SimRank:
     # Graph.csv path
     GRAPH_DATA_PATH = pathlib.Path.cwd() / 'Movielens_data' / 'graph.csv'
 
-    """Loads graph.csv and creates a sub graph of the given users
-    
-    Parameters:
-    u1 (String): User ID of user 1
-    u2 (String): User ID of user 2
-    path (String): Path to the graph.csv file
-    
-    Returns:
-    NetworkX Graph Object"""
     @staticmethod
     def load_graph_users(u1, u2, path):
-        if int(u1) >= int(u2):
-            maximal = int(u1)
+        """Loads graph.csv and creates a sub graph of the given users
+
+        Parameters:
+        u1 (Integer): User ID of user 1
+        u2 (Integer): User ID of user 2
+        path (String): Path to the graph.csv file
+
+        Returns:
+        NetworkX Graph Object"""
+        u1_string = "U:" + str(u1)
+        u2_string = "U:" + str(u2)
+        if u1 >= u2:
+            max_val = u1
         else:
-            maximal = int(u2)
+            max_val = u2
         with open(path) as file:
             next(file)
             graph = nx.Graph()
             for lines in file:
-                splitlines = lines.split(",")
-                if splitlines[1] == u1:
-                    graph.add_edge(u1, "M"+splitlines[0], weight=splitlines[2])
-                elif splitlines[1] == u2:
-                    graph.add_edge(u2, "M"+splitlines[0], weight=splitlines[2])
+                file_line_split = lines.split(",")
+                if file_line_split[1] == u1_string:
+                    graph.add_edge(u1_string, file_line_split[0], weight=file_line_split[2])
+                elif file_line_split[1] == u2_string:
+                    graph.add_edge(u2_string, file_line_split[0], weight=file_line_split[2])
                 else:
-                    if maximal < int(splitlines[1]):
+                    if max_val < int(file_line_split[1].split(":")[1]):
                         break
                     continue
             return graph
 
-    """Given 2 users returns the similarity of them using NetworkX SimRank
-    
-    Parameters:
-    user1 (String): User ID of user 1
-    user2 (String): User ID of user 2
-    
-    Returns:
-    int: Similarity of user 1 and user 2"""
     def similarity(self, user1, user2):
+        """Given 2 users returns the similarity of them using NetworkX SimRank
+
+        Parameters:
+        user1 (String): User ID of user 1
+        user2 (String): User ID of user 2
+
+        Returns:
+        int: Similarity of user 1 and user 2"""
         graph = self.load_graph_users(user1, user2, self.GRAPH_DATA_PATH)
-        similarity = nx.algorithms.similarity.simrank_similarity(graph, user1, user2)
+        similarity = nx.algorithms.similarity.simrank_similarity(graph, "U:"+str(user1), "U:"+str(user2))
         return similarity
+
+f = SimRank()
+f.similarity(1, 2)
