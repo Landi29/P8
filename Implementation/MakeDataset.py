c48520104ab91194e9e21dc911bc66b2c43d4493
Andreas Dahl Nielsen <andnie16@student.aau.dk>
2020-04-14 09:09:25 +0200

    Update MakeDataset.py

--- Implementation/MakeDataset.py
+++ Implementation/MakeDataset.py
@@ -1,17 +1,18 @@
 """Contains functionality for creating the dataset used in SimGNN"""
 import pathlib
 from tqdm import tqdm
+from SimGNN import SimGNN_Trainer
 
 class SimGNNDatasetCreator:
     GRAPH_DATA_PATH = pathlib.Path.cwd() / 'Movielens_data' / 'graph.csv'
     GRAPHLIST_PATH = pathlib.Path.cwd() / 'Movielens_data' / 'graphlists.edgelist'
     JSON_FILE = pathlib.Path.cwd() / 'Movielens_data' / 'training.pkl'
-    def __init__(self):
+
+    def make_dataset(self):
         """
-        INIT file for SimGNNDatasetCreator.
         Will from the graph.csv file which contains an edgelist create every user graph
         It will then use these values to create the dataset used for SimGNN.
-        Currently the amount of data entries per user is set to 10
+        Currently the amount of data entries per user is set to 5
         """
         with open(self.GRAPH_DATA_PATH, "r") as file:
             next(file)
@@ -20,19 +21,25 @@ class SimGNNDatasetCreator:
             for lines in file:
                 lines_split = lines.split(",")
                 if lines_split[1] in allgraphs:
-                    allgraphs[lines_split[1]].append([lines_split[0], lines_split[1], lines_split[2]])
+                    allgraphs[lines_split[1]].append(self.get_new_graph(lines_split))
                     labels[lines_split[1]].append(lines_split[0])
                 else:
                     allgraphs[lines_split[1]] = []
                     labels[lines_split[1]] = []
-                    allgraphs[lines_split[1]].append([lines_split[0], lines_split[1], lines_split[2]])
+                    allgraphs[lines_split[1]].append([lines_split[1], "High"])
+                    allgraphs[lines_split[1]].append([lines_split[1], "Medium"])
+                    allgraphs[lines_split[1]].append([lines_split[1], "Low"])
+                    allgraphs[lines_split[1]].append(self.get_new_graph(lines_split))
+                    labels[lines_split[1]].append("Medium")
+                    labels[lines_split[1]].append("Low")
+                    labels[lines_split[1]].append("High")
                     labels[lines_split[1]].append(lines_split[1])
                     labels[lines_split[1]].append(lines_split[0])
             dataset = {}
             for x in tqdm(range(1, allgraphs.__len__())):
                 user1 = "U:" + str(x)
                 dataset[user1] = {}
-                for y in range(x+1, x+11):
+                for y in range(x + 1, x + 6):
                     try:
                         user2 = "U:" + str(y)
                         dataset[user1][user2] = {}
@@ -43,6 +50,20 @@ class SimGNNDatasetCreator:
                         dataset[user1][user2]["GED"] = self.my_find_ged(labels[user1], labels[user2], allgraphs[user1], allgraphs[user2])
                     except:
                         break
+        return allgraphs
+
+
+    @staticmethod
+    def get_new_graph(graph):
+        edgelist = []
+        value = float(graph[2].strip())
+        if value >= 4.0:
+            edgelist.append(["High", graph[0]])
+        elif value >= 2.0:
+            edgelist.append(["Medium", graph[0]])
+        else:
+            edgelist.append(["Low", graph[0]])
+        return edgelist
 
     @staticmethod
     def my_find_ged(labels1, labels2, graph1, graph2):
@@ -60,8 +81,10 @@ class SimGNNDatasetCreator:
         gedscore (integer): Graph Edit Distance for graph1 and graph2
         """
         gedscore = 0
+        similarnodes = []
         for node in labels1:
             if labels2.__contains__(node):
+                similarnodes.append(node)
                 continue
             else:
                 gedscore += 1
@@ -70,8 +93,22 @@ class SimGNNDatasetCreator:
                 continue
             else:
                 gedscore += 1
+        for node in similarnodes:
+            i1 = 0.0
+            i2 = 0.0
+            for node1 in graph1:
+                if node1[0] == node:
+                    i1 = float(node1[2].strip())
+                    break
+            for node1 in graph2:
+                if node1[0] == node:
+                    i2 = float(node1[2].strip())
+                    break
+            gedscore += abs(i1-i2)
         return gedscore
 
 
-SimGNNDatasetCreator()
+f = SimGNNDatasetCreator()
+dataset = f.make_dataset()
+SimGNN_Trainer(dataset)
 JSON_FILE = pathlib.Path.cwd() / 'Movielens_data' / 'training.json'
