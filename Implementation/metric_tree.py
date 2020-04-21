f79b41d004bc9ebfe5417e7d7a3d227936e3e032
Lars Svane Jensen <lsje16@student.aau.dk>
2020-04-21 14:45:46 +0200

    Update Implementation/metric_tree.py
    Co-Authored-By: Sebastian R. Petersen <sebpetersen@outlook.com>

--- Implementation/metric_tree.py
+++ Implementation/metric_tree.py
@@ -50,7 +50,7 @@ def get_random_pair(data):
 
 def split_data(masterdata, splitpoint1, splitpoint2):
     '''
-    description: this function splits data in two according to the distance to the split points
+    description: This function splits data in two according to the distance to the split points
     parameters: masterdata is the data you wish to split splitpoint1 and splitpoint2 is
     the entities you wish to split around
     and k is the number of entities you wish to find
