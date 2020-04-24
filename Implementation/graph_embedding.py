dd94a48fbf90f6ee69befd309a61152b3633b4a9
Aryan <amoha16@student.aau.dk>
2020-04-24 19:15:23 +0200

    Started to create the graph.

--- Implementation/graph_embedding.py
+++ Implementation/graph_embedding.py
@@ -1,11 +1,24 @@
 from node2vec import Node2Vec
 import networkx
+import Paths
 
-graph = networkx.Graph()
 
 
-# Generate walks
-node2vec = Node2Vec(graph, dimensions=20, walk_length=16, num_walks=100)
+def create_graph(data_path):
+    with open(data_path, "r") as data:
+        ratings = data.readlines()
+    graph = networkx.Graph()
 
-# Learn embeddings 
-model = node2vec.fit(window=10, min_count=1)
+    return graph
+
+
+
+
+
+if __name__ == "__main__":
+    graph = create_graph(Paths.SMALL_GRAPH_RATINGS_PATH)
+    # Generate walks
+    node2vec = Node2Vec(graph, dimensions=20, walk_length=16, num_walks=100)
+
+    # Learn embeddings 
+    model = node2vec.fit(window=10, min_count=1)
