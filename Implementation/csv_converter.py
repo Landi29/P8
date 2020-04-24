7aec89ecec16fecb20a0d1491640f88b4ed39ab7
Aryan <amoha16@student.aau.dk>
2020-04-24 18:46:32 +0200

    Changed file option.

--- Implementation/csv_converter.py
+++ Implementation/csv_converter.py
@@ -2,6 +2,6 @@ import csv
 import Paths
 with open (Paths.SMALL_GRAPH_RATINGS_PATH) as data:
     ratings = data.readlines()
-with open(Paths.SMALL_GRAPH_RATINGS_CSV_PATH,'wb') as output:
+with open(Paths.SMALL_GRAPH_RATINGS_CSV_PATH,'w+') as output:
     writer = csv.writer(output)
     writer.writerows(ratings)
