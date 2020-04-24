2c17304048d4ea37fa2f07ac1242f7d6dc9131e9
Aryan <amoha16@student.aau.dk>
2020-04-24 18:50:55 +0200

    Added tqdm.

--- Implementation/csv_converter.py
+++ Implementation/csv_converter.py
@@ -1,7 +1,15 @@
 import csv
 import Paths
+from tqdm import tqdm
+
+print("Reading the data.")
+
 with open (Paths.SMALL_GRAPH_RATINGS_PATH) as data:
     ratings = data.readlines()
+
+print("Converting the data to a csv file.")
+
 with open(Paths.SMALL_GRAPH_RATINGS_CSV_PATH,'w+') as output:
     writer = csv.writer(output)
-    writer.writerows(ratings)
+    for rating in tqdm(ratings):
+        writer.writerow(ratings)
