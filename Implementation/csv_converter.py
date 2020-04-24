8a6faa6f441272a2ff55c4b00af8ac7f7fb293a4
Aryan <amoha16@student.aau.dk>
2020-04-24 19:00:25 +0200

    Changed how we write to the file.

--- Implementation/csv_converter.py
+++ Implementation/csv_converter.py
@@ -4,11 +4,11 @@ from tqdm import tqdm
 
 print("Reading the data.")
 
-with open (Paths.SMALL_GRAPH_RATINGS_PATH) as data:
-    ratings = data.readlines()
+ratings = [i.strip().split() for i in open(Paths.SMALL_GRAPH_RATINGS_PATH).readlines()]
 
 print("Converting the data to a csv file.")
 
 with open(Paths.SMALL_GRAPH_RATINGS_CSV_PATH,'w+') as output:
     writer = csv.writer(output)
+    
     writer.writerows(ratings)
