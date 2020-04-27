6a240f798361f9afa43c1574c3840591801b004a
Aryan <amoha16@student.aau.dk>
2020-04-27 09:15:34 +0200

    Running setting 2.

--- Implementation/graph_embedding.py
+++ Implementation/graph_embedding.py
@@ -4,10 +4,18 @@ import Paths
 from tqdm import tqdm
 import pickle
 from datetime import datetime
+
 def create_graph(data_path):
+    '''
+    Description
+    -----------
+    Create a networkx graph based on the data of the form: userid::movieid::rating
+
+    Parameters
+    ----------
+    `data_path`: File path to the graph data. 
+    '''
     graph = networkx.Graph()
-    #with open(data_path, "r") as data:
-        #ratings = [next(data) for x in range(100000)]
     with open(data_path, "r") as data:
         ratings = data.readlines()
     
@@ -17,12 +25,23 @@ def create_graph(data_path):
     return graph
 
 def print_graph_information(number_of_nodes, number_of_edges):
+    '''
+    Description
+    -----------
+    A collection of print statemements for a graph. This is to increase the readability of the code and execution of it.
+
+    Parameters
+    ----------
+    number_of_nodes: The number of nodes in the graph.
+    number_of_edges: The number of edges in the graph.
+    '''
     print("Graph created.")
     print("Graph information:")
     print("Number of nodes: {}".format(number_of_nodes))
     print("Number of edges: {}".format(number_of_edges))
 
 if __name__ == "__main__":
+
     print("Start at: {}".format(datetime.now().strftime("%d-%m-%Y %H:%M:%S")))
     print("Reading the file: ")
     graph = create_graph(Paths.SMALL_GRAPH_RATINGS_PATH)
@@ -30,13 +49,14 @@ if __name__ == "__main__":
     
     # Generate walks
     print("Generate walks:")
-    node2vec = Node2Vec(graph, dimensions=20, walk_length=16, num_walks=100)
-    #graphn2v = Node2Vec(graph, dimensions= 20, walk_length=20, num_walks=50, p=1, q=2, workers=4)
+    #node2vec = Node2Vec(graph, dimensions=20, walk_length=16, num_walks=100)
+
+    graphn2v = Node2Vec(graph, dimensions= 20, walk_length=20, num_walks=50, p=1, q=2, workers=4)
 
     # Learn embeddings 
     print("Learning embeddings:")
-    nv2model = node2vec.fit(window=10, min_count=1)
+    nv2model = graphn2v.fit(window=10, min_count=1)
     print("nv2model is written to disc")
-    with open(Paths.SMALL_N2V_MODEL_PATH, "wb") as disc_file:
+    with open(Paths.SMALL_N2V_MODEL_PATH_2, "wb") as disc_file:
         pickle.dump(nv2model, disc_file) 
     print("Finished at: {}".format(datetime.now().strftime("%d-%m-%Y %H:%M:%S")))
