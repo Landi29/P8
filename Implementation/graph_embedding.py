d177ae05e946f9a505485e361a9a2ad98ba0323e
Aryan <amoha16@student.aau.dk>
2020-04-28 22:18:45 +0200

    Added support for json fold files.

--- Implementation/graph_embedding.py
+++ Implementation/graph_embedding.py
@@ -2,6 +2,7 @@
 Module for embedding the movielens dataset with 1 million ratings
 using node2vec.
 '''
+import json
 import pickle
 from datetime import datetime
 import networkx
@@ -12,7 +13,7 @@ import pathlib
 import csv
 
 
-def create_graph(data_path):
+def create_graph_csv(data_path):
     '''
     Description
     -----------
@@ -26,6 +27,20 @@ def create_graph(data_path):
     with open(data_path, "r") as data:
         csvreader = csv.reader(data)
         for edge in csvreader:
+            graph.add_edge(int(edge[0].replace("M:","1")), 
+                           int(edge[1].replace("U:","2")), weight=float(edge[2]))
+    return graph
+
+
+def create_graph_from_folds(data_path, amount_of_folds):
+    graph = networkx.Graph()
+    with open(data_path, "r") as read_file:
+        graph_data = json.load(read_file)
+    keys = list(graph_data)
+    # For each of the folds    
+    for i in range(amount_of_folds -1 ):
+        # Keys[i] returns the key for the given fold
+        for edge in graph_data[keys[i]]:
             graph.add_edge(int(edge[0].replace("M:","1")), 
                            int(edge[1].replace("U:","2")), weight=float(edge[2]))
     return graph
@@ -58,10 +73,16 @@ if __name__ == "__main__":
     if filetype == "csv":
         print("Start at: {}".format(datetime.now().strftime("%d-%m-%Y %H:%M:%S")))
         print("Reading the file: ")
-        GRAPH = create_graph(filepath)
+        GRAPH = create_graph_csv(filepath)
+        print_graph_information(GRAPH.number_of_nodes(), GRAPH.number_of_edges())
+    elif filetype == "json":
+        folds_to_train = input("Enter the amount of folds you want for training the model: ")
+        print("Start at: {}".format(datetime.now().strftime("%d-%m-%Y %H:%M:%S")))
+        print("Reading the file: ")
+        GRAPH = create_graph_from_folds(filepath, folds_to_train)
         print_graph_information(GRAPH.number_of_nodes(), GRAPH.number_of_edges())
     else: 
-        print("You did not specify a csv file.")
+        print("You did not specify a csv or json file.")
         
     # Generate walks
     print("Generate walks:")
