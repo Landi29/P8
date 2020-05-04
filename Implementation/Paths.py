27e21dc11c1d698e70e3989b137b3d061c570330
Andreas Dahl Nielsen <andnie16@student.aau.dk>
2020-05-04 13:46:33 +0200

    Update Paths.py

--- Implementation/Paths.py
+++ Implementation/Paths.py
@@ -1,5 +1,7 @@
 import pathlib
 
+FOLDS_DATA_PATH = pathlib.Path.cwd() / 'Movielens_data' / 'Folds_100k.json'
+SIMGNN_DATA_PATH = pathlib.Path.cwd() / 'Movielens_data' / 'SimGNN'
 MOVIE_NODES_PATH = pathlib.Path.cwd() / 'Movielens_data' / 'movie_nodes.csv'
 USER_NODES_PATH = pathlib.Path.cwd() / 'Movielens_data' / 'user_nodes.csv'
 GRAPH_DATA_PATH = pathlib.Path.cwd() / 'Movielens_data' / 'graph.csv'
